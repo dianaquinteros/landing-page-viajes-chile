409459b38fe5f7abdf8e27956cf5910aba97b86e
dianaquinteros <di.quinterosg@gmail.com>
2021-07-11 20:11:19 -0400

    Añadir funciones js, smoothScroll y cambiar color de navbar al hacer scroll

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+$(function () {
+    // Habilita links de navbar hacia las secciones del sitio
+    $('a').click(function (event) {
+        event.preventDefault();
+
+        var gato = this.hash;
+
+        $('html').animate(
+            {
+                scrollTop: $(gato).offset().top
+            },
+            800
+        );
+    });
+
+    // Cambia color de navbar de transparente a negro cuando se realiza scroll en la página
+    $(document).scroll(function () {
+        var $nav = $(".navbar.fixed-top");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+    
+});
